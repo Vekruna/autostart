5205dd9d7c9939a469e93b4dc9235d6887ade0f0
Vadym <152736495+Vekruna@users.noreply.github.com>
2026-04-02 14:29:48 +0000

    Create .autostart.sh

---
+++ .autostart.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+while true; do
+
+        STAT=$(cat /sys/class/power_supply/BAT0/status)
+        CAP=$(cat /sys/class/power_supply/BAT0/capacity)
+        xsetroot -name " BAT: $CAP% [$STAT] | $(date)"
+        sleep 1s
+done &
